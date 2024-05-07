5ba604688e798ebbdd4d94dc0a1335aa3b54f36a
Ricardo <ricardo.palma.rjp@gmail.com>
2024-05-06 21:52:27 -0500

    Added per user image upload

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,11 @@
 
 import { sql } from "drizzle-orm";
 import {
-  index,
-  pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
+    index,
+    pgTableCreator,
+    serial,
+    timestamp,
+    varchar,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -19,17 +19,18 @@ import {
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
 export const images = createTable(
-  "images",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }).notNull(),
-    url: varchar("url", {length: 1024}).notNull(),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+    "images",
+    {
+        id: serial("id").primaryKey(),
+        name: varchar("name", { length: 256 }).notNull(),
+        url: varchar("url", { length: 1024 }).notNull(),
+        userId: varchar("userid", { length: 256 }).notNull(),
+        createdAt: timestamp("created_at")
+            .default(sql`CURRENT_TIMESTAMP`)
+            .notNull(),
+        updatedAt: timestamp("updatedAt"),
+    },
+    (example) => ({
+        nameIndex: index("name_idx").on(example.name),
+    })
 );
